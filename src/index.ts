e9b6689fa7c19511d558c6a1fa404d49d3c4b209
dipam-2123 <dipam005k@gmail.com>
2026-02-16 23:50:24 +0530

    implement fetch teacher and create classes

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,41 @@
 import AgentAPI from "apminsight";
 AgentAPI.config()
 
-import express from "express";
+import express from 'express';
 import cors from "cors";
 
-import subjectsRouter from "./routes/subjects.js"
+import subjectsRouter from "./routes/subjects.js";
+import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
 import securityMiddleware from "./middleware/security.js";
-
+import {toNodeHandler} from "better-auth/node";
 import {auth} from "./lib/auth.js";
-import {toNodeHandler} from "better-auth/node"
 
 const app = express();
-const PORT= 8000;
+const PORT = 8000;
 
-if(!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set ion .env file')
+if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    methods:['GET','POST','PUT','DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}));
+}))
 
-app.all('/api/auth/*splat', toNodeHandler(auth));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
+app.use('/api/subjects', subjectsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/classes', classesRouter)
+
 app.use(securityMiddleware);
 
-app.use('/api/subjects', subjectsRouter)
-
-app.get("/", (req, res) => {
-    res.send('Hello, welcome to the classroom API!');
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to the Classroom API!');
 });
 
-app.listen(PORT,() => {
-    console.log(`Server is running http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
